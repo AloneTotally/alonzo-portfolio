0c017a58908e0c500b0382d04ddc7d9f47a2c699
AloneTotally <alonzothenoob@gmail.com>
2023-01-04 14:57:28 +0800

    About me page (Without links)

---
+++ src/lib/Aboutme.svelte
@@ -0,0 +1,19 @@
+<div class="text-white flex flex-col items-center text-lg">
+	<h2 class="font-bold text-4xl mt-10">About me</h2>
+	<span class="text-slate-400">My introduction</span>
+	<div class="flex mt-16">
+		<img class="rounded-full w-48 h-48" src="https://picsum.photos/400/400" alt="" />
+		<div class="grid justify-center flex-col max-w-md ml-10">
+			<div>I am a student at the school of science and technology.</div>
+			<div class="">
+				While I was there I worked on projects and gained some knowledge of programming from there,
+				most of which is self-taught.
+			</div>
+			<div class="text-slate-400">Personal email: alonzothenoob@gmail.com</div>
+			<div class="flex">
+				<a href="#">Link to github</a>
+				<a href="#">Link to linkedin?</a>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/lib/Projects.svelte
@@ -0,0 +1,4 @@
+<div class="text-white flex flex-col items-center text-lg">
+	<h2 class="font-bold text-4xl mt-10">Projects</h2>
+	<span class="text-slate-400">Projects that I have coded up/designed</span>
+</div>
