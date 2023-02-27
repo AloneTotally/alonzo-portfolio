59ba3b79ec0e8813276de67ef8199065fa854e3b
AloneTotally <alonzothenoob@gmail.com>
2023-02-27 17:05:30 +0800

    added the svg for github and linkedin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Aboutme from '$lib/Aboutme.svelte';
+	import Projects from '$lib/Projects.svelte';
 </script>
 
 <body class="bg-slate-900 flex flex-col justify-center content-center text-white">
 	<Aboutme />
+	<Projects />
 </body>
