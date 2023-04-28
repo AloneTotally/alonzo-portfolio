841e64850ddea8a2b3fa58a335f95843588497f3
AloneTotally <alonzothenoob@gmail.com>
2023-04-28 23:17:33 +0800

    changed og tags again

--- src/lib/Aboutme.svelte
+++ src/lib/Aboutme.svelte
@@ -2,9 +2,9 @@
 	<div class="flex justify-center flex-col xl:pl-56 md:pl-10" id="aboutme">
 		<div class="text-5xl font-bold w-full">Hi, I am Alonzo Puah</div>
 		<div class="text-2xl font-bold w-full text-slate-300 mt-3">SST student in Singapore</div>
-		<div class="mt-10 w-9/12">
-			I worked on projects and gained some knowledge of programming from SST, most of which is
-			self-taught.
+		<div class="mt-5 w-9/12">
+			At SST, I worked on web projects and gained some knowledge of programming and web development,
+			most of which is self-taught.
 		</div>
 
 		<a
@@ -13,7 +13,7 @@
 		>
 	</div>
 
-	<div class="min-h-screen flex items-center justify-center xl:pr-56 sm:pr-10">
+	<div class="min-h-screen flex items-center justify-center xl:pr-52 sm:pr-10">
 		<img class="rounded-full h-auto w-3/4" src="../../myself.jpg" alt="" />
 	</div>
 </div>
